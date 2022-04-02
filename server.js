34eebdbbf3461f6da169337cead19372ec33be12
Ahmed <ahmadelshafie1998@hotmail.com>
2022-04-02 17:06:56 -0300

    Redirect to https if necessary

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ import express from 'express';
 import path from 'path';
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 8000
+var app = express()
 
-express()
+app.enable('trust proxy');
+
+app.use (function (req, res, next) {
+	if (req.secure || req.hostname == "localhost") {
+		next();
+	} else {
+		res.redirect('https://' + req.headers.host + req.url);
+	}
+});
+
+app
 .use('/', express.static(__dirname))
 .listen(PORT, () => console.log(`Listening on ${PORT}`));
